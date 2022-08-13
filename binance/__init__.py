b186e2adaf6035eab112a172ba0bad08de7a5612
Saurabh Badenkal <saurabhbadenkalat@gmail.com>
2022-08-13 14:40:09 -0700

    Revert "imported binance from local lib path"
    This reverts commit 37f5132a84471021b3f1e7315d20e8bd40079da4.

--- binance/__init__.py
+++ binance/__init__.py
@@ -6,6 +6,6 @@
 
 __version__ = '1.0.16'
 
-from bot.lib.python_binance.binance.client import Client, AsyncClient  # noqa
-from bot.lib.python_binance.binance.depthcache import DepthCacheManager, OptionsDepthCacheManager, ThreadedDepthCacheManager  # noqa
-from bot.lib.python_binance.binance.streams import BinanceSocketManager, ThreadedWebsocketManager  # noqa
+from binance.client import Client, AsyncClient  # noqa
+from binance.depthcache import DepthCacheManager, OptionsDepthCacheManager, ThreadedDepthCacheManager  # noqa
+from binance.streams import BinanceSocketManager, ThreadedWebsocketManager  # noqa
--- binance/helpers.py
+++ binance/helpers.py
@@ -6,7 +6,7 @@ import pytz
 
 from datetime import datetime
 
-from .exceptions import UnknownDateFormat
+from binance.exceptions import UnknownDateFormat
 
 
 def date_to_milliseconds(date_str: str) -> int:
