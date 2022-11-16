692e4e588ee1e1bf03c684eab6cc9bc236eb2681
Saurabh Badenkal <saurabhbadenkalat@gmail.com>
2022-11-16 11:27:08 -0800

    code not in json_res Binance exceptions.py

--- binance/exceptions.py
+++ binance/exceptions.py
@@ -11,8 +11,8 @@ class BinanceAPIException(Exception):
         except ValueError:
             self.message = 'Invalid JSON error message from Binance: {}'.format(response.text)
         else:
-            self.code = json_res['code']
-            self.message = json_res['msg']
+            self.code = json_res['code'] if 'code' in json_res else '-1'
+            self.message = json_res['msg'] if 'msg' in json_res else 'msg not in json_res'
         self.status_code = status_code
         self.response = response
         self.request = getattr(response, 'request', None)
